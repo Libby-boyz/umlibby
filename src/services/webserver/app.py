b9740153e69376df19fdde646852e5e436894f82
Logan <logand007@outlook.com>
2026-02-20 14:30:31 -0600

    idek

--- src/services/webserver/app.py
+++ src/services/webserver/app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+import mysql.connector
+import signal
+import sys
 
 app = Flask(__name__)
-
-process.on('SIGTERM', () => shutdown('SIGTERM'));
+signal.signal(signal.SIGTERM, lambda : sys.exit(5))
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
