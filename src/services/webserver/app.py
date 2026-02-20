fc7e3c082dee60995c44c38b536234918bc9fef7
sossh <112524005+sossh@users.noreply.github.com>
2026-02-20 17:40:01 -0600

    get library count

--- src/services/webserver/app.py
+++ src/services/webserver/app.py
@@ -3,6 +3,7 @@ import mysql.connector
 import signal
 import sys
 import os
+import datetime
 
 app = Flask(__name__)
 signal.signal(signal.SIGTERM, lambda: sys.exit(5))
@@ -46,3 +47,47 @@ def newDatapoint(id, enter: bool, cursor):
             'INSERT INTO libby.capacity_datapoint (library_id, direction) VALUES (%s, %s)'
     )
     cursor.execute(statement, (id, enter))
+
+
+
+
+@app.route('/api/libraries/<int:library_id>')
+def get_library(library_id:int):
+   
+    
+    # Current Count
+    cursor = connection.cursor()
+    count = get_current_library_count(library_id, cursor)
+    cursor.close()
+    return count
+
+def get_current_library_count(library_id, cursor)->int:
+    today_start = datetime.datetime.today()
+    statement = (
+        '''
+        with today as(
+        select * from 
+        libby.library natural join libby.capacity_datapoint
+        where library.id = %s and time > %s
+        ),
+        ins as(
+        select count(*) as inCOunt from today
+        where direction = true
+        ),
+        outs as(
+        select count(*) as outCount from today
+        where direction = false
+        )
+        select inCount - (select outCount from outs) from ins
+        ''' 
+    )
+    cursor.execute(statement, (library_id, today_start))
+    result = cursor.fetchone()
+    
+    # Make sure we actually got a value
+    if result and result[0] is not None:
+        return int(result[0])
+    
+    return 0
+
+
