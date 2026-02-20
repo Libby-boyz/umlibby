51fb3aff1e982812b22379e165d4b9a728354cb3
cjlangan <connorjameslangan@gmail.com>
2026-02-20 14:29:55 -0600

    added MUI

--- src/services/frontend/src/app/main.tsx
+++ src/services/frontend/src/app/main.tsx
@@ -4,14 +4,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Home from "./home.tsx";
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <BrowserRouter>
             <Routes>
-                <Route
-                    path="/"
-                    element={<Home />}
-                />
+                <Route path="/" element={<Home />} />
             </Routes>
         </BrowserRouter>
     </StrictMode>,
