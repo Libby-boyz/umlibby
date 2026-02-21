340f27b2dfe0f648693d518668b7faa7a8c8e215
cjlangan <connorjameslangan@gmail.com>
2026-02-21 10:54:31 -0600

    images on frontend

--- src/services/frontend/src/components/card.tsx
+++ src/services/frontend/src/components/card.tsx
@@ -1,44 +1,26 @@
 import type { ILibrary } from "@mytypes/library";
 
 export default function Card({ name, building, floor_count, capacity, image, id, fullness }: ILibrary) {
-  
+
   if(!image || image.trim().length === 0){
     image = `static/${id}.png`
   }
 
-  // On load of the page, fetch the data from the backend
-  // useEffect(() => {
-
-  //     const load = async () => {
-  //         await fetch(`${apiBaseUrl}/api/libraries/${id}/today/count`)
-  //           .then((res) => {
-  //             if(!res.ok) {
-  //                 throw new Error("Network response was not ok");
-  //             }
-  //             return res.json();
-  //           })
-  //           .then((data) => {
-  //             console.log("results:", data)
-  //             setCurrent(data.count);
-  //           })
-  //           .catch((error) => {
-  //             console.error("Error fetching data:", error);
-  //           })
-  //     }
-
-  //     load();
-  // }, []);
-  
   return (
-    <div className="max-w rounded-2xl overflow-hidden shadow-lg bg-light-blue m-5">
-      <div className="p-4">
-        <h2 className="text-3xl font-semibold mb-2">{name}</h2>
-          <div className="text-lg font-medium">
-            <p className="">Capacity: {fullness == null ? 0 : fullness}/{capacity}</p>
-            <p className="">Building: {building}</p>
-            <p className="">Floor Count: {floor_count}</p>
-          </div>
+    <div className="flex max-w-3xl rounded-2xl overflow-hidden shadow-lg bg-light-blue m-5">
+      <div className="flex-1 p-6 flex flex-col justify-center">
+        <h2 className="text-3xl font-semibold mb-4">{name}</h2>
+        <div className="text-lg space-y-1">
+          <p>Capacity: {fullness == null ? 0 : fullness}/{capacity}</p>
+          <p>Building: {building}</p>
+          <p>Floor Count: {floor_count}</p>
+        </div>
       </div>
+      <img
+        className="w-80 object-cover"
+        src={image || "https://umanitoba.ca/libraries/sites/libraries/files/styles/3x2_900w/public/2020-11/dafoe-library.JPG?itok=77qX113_"}
+        alt={name}
+      />
     </div>
   );
 }
