e3d212ff04961221c1f795d13eb03fb08c964345
sossh <112524005+sossh@users.noreply.github.com>
2026-02-21 10:52:36 -0600

    some images

--- src/services/db/schema.sql
+++ src/services/db/schema.sql
@@ -23,12 +23,14 @@ CREATE TABLE libby.capacity_datapoint(
 
 
 INSERT INTO libby.library (name, building, latitude, longitude, capacity, floor_count) VALUES
-    ("Elizabeth Dafoe Library",                        "Elizabeth Dafoe Library",    49.80588, -97.14097, 1007, 3),
-    ("Father Harold Drake Library",                    "St. Paul's College",         49.80806, -97.13000, 150 , 2),
-    ("E. K. Williams Law Library",                     "Robson Hall",                49.80550, -97.14150, 113,  1),
+    ("Elizabeth Dafoe Library",                        "Elizabeth Dafoe Library",    49.80588, -97.14097, 1007, 3, "https://umanitoba.ca/libraries/sites/libraries/files/styles/3x2_900w/public/2020-11/dafoe-library.JPG?itok=77qX113_"),
+    ("Father Harold Drake Library",                    "St. Paul's College",         49.80806, -97.13000, 150 , 2, "https://umanitoba.ca/libraries/sites/libraries/files/styles/3x2_900w/public/2020-12/father-h-drake-library_2.jpg?itok=Y_svr4zg"),
+    ("E. K. Williams Law Library",                     "Robson Hall",                49.80550, -97.14150, 113,  1, "https://www.umanitoba.ca/libraries/sites/libraries/files/styles/3x2_900w/public/2020-12/ek-williams-law-library-exterior.jpg?itok=6b-G8tAl"),
     ("Architecture/Fine Arts Library",                 "John A. Russell Building",   49.80450, -97.13700, 63,   1),
     ("Jim Peebles Sciences and Technology Library",    "Machray Hall",               49.80600, -97.14050, 486,  3),
     ("Albert D. Cohen Management Library",             "Drake Centre",               49.80806, -97.13000, 0,    4),
     ("Donald W. Craik Engineering Library",            "EITC-E3",                    49.80590, -97.14000, 0,    1),
     ("Eckhardt-Gramatte Music Library",                "Tache Arts Complex",         49.80580, -97.14200, 0,    1),
     ("St. John's College Library",                     "St. John's College",         49.80480, -97.13850, 0,    1);
+
+    
