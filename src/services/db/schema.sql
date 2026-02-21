e577b0cce79859d99d65fb89325a52d93d8e97be
sossh <112524005+sossh@users.noreply.github.com>
2026-02-21 11:52:13 -0600

    New Images

--- src/services/db/schema.sql
+++ src/services/db/schema.sql
@@ -30,7 +30,6 @@ INSERT INTO libby.library (name, building, latitude, longitude, capacity, floor_
     ("E. K. Williams Law Library",                     "Robson Hall",                49.80550, -97.14150, 113,  1, "https://www.umanitoba.ca/libraries/sites/libraries/files/styles/3x2_900w/public/2020-12/ek-williams-law-library-exterior.jpg?itok=6b-G8tAl", "ChIJQ03Rg8N16lIRCSg0XpMVN48"),
     ("Architecture/Fine Arts Library",                 "John A. Russell Building",   49.80450, -97.13700, 63,   1, " ", "ChIJw1aL5_p16lIR3VZwVaUxt4c"),
     ("Jim Peebles Sciences and Technology Library",    "Machray Hall",               49.80600, -97.14050, 486,  3, "static/5.png", "ChIJU0VlYeN16lIR7G_0aqtCyIc"),
-    ("Albert D. Cohen Management Library",             "Drake Centre",               49.80806, -97.13000, 0,    4, " ", ""),
-    ("Donald W. Craik Engineering Library",            "EITC-E3",                    49.80590, -97.14000, 0,    1, " ", ""),
-    ("Eckhardt-Gramatte Music Library",                "Tache Arts Complex",         49.80580, -97.14200, 0,    1, "https://umanitoba.ca/libraries/sites/libraries/files/styles/3x2_900w/public/2024-12/music-library-interior-study-area.jpg?itok=U8kfJFra", "ChIJ47NlLRZ16lIRmX5v8E3uUMs"),
-    ("St. John's College Library",                     "St. John's College",         49.80480, -97.13850, 0,    1, " ", "");
+    ("Albert D. Cohen Management Library",             "Drake Centre",               49.80806, -97.13000, 0,    4, "static/6.png", ""),
+    ("Donald W. Craik Engineering Library",            "EITC-E3",                    49.80590, -97.14000, 0,    1, "static/7.png", ""),
+    ("Eckhardt-Gramatte Music Library",                "Tache Arts Complex",         49.80580, -97.14200, 0,    1, "https://umanitoba.ca/libraries/sites/libraries/files/styles/3x2_900w/public/2024-12/music-library-interior-study-area.jpg?itok=U8kfJFra", "ChIJ47NlLRZ16lIRmX5v8E3uUMs")
