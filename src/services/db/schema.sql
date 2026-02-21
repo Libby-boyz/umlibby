21358d55cc9fd5e01ec779ae0426e484614e6a03
sossh <112524005+sossh@users.noreply.github.com>
2026-02-21 02:00:24 -0600

    Some Library capacities found

--- src/services/db/schema.sql
+++ src/services/db/schema.sql
@@ -23,12 +23,13 @@ CREATE TABLE libby.capacity_datapoint(
 
 
 INSERT INTO libby.library (name, building, latitude, longitude, capacity, floor_count) VALUES
+    ("Elizabeth Dafoe Library",            "none",                       49.80588, -97.14097, 1,007, 3),
+    ("Father Harold Drake Library", "St. Paul's College",               49.80806, -97.13000, 150 , 2),
+    ("E. K. Williams Law Library",         "Robson Hall",                49.80550, -97.14150, 113, 1),
+    ("Architecture/Fine Arts Library",     "John A. Russell Building",   49.80450, -97.13700, 63, 1),
+    ("Jim Peebles Sciences and Technology Library",    "Machray Hall",    49.80600, -97.14050, 486, 3),
+
     ("Albert D. Cohen Management Library", "Drake Centre",               49.80806, -97.13000, 0, 4),
-    ("Architecture/Fine Arts Library",     "John A. Russell Building",   49.80450, -97.13700, 0, 1),
     ("Donald W. Craik Engineering Library","EITC-E3",                    49.80590, -97.14000, 0, 1),
-    ("E. K. Williams Law Library",         "Robson Hall",                49.80550, -97.14150, 0, 1),
     ("Eckhardt-Gramatte Music Library",    "Tache Arts Complex",         49.80580, -97.14200, 0, 1),
-    ("Elizabeth Dafoe Library",            "none",                       49.80588, -97.14097, 0, 3),
-    ("Fr. Harold Drake Library",           "St. Paul's College",         49.80560, -97.14150, 0, 2),
-    ("Sciences and Technology Library",    "Machray Hall",               49.80600, -97.14050, 0, 1),
     ("St. John's College Library",         "St. John's College",         49.80480, -97.13850, 0, 1);
