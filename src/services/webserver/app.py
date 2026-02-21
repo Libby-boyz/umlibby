8197507e913cdeca301a9170c66352bd212d142b
Logan <logand007@outlook.com>
2026-02-21 10:55:30 -0600

    cleanup

--- src/services/webserver/app.py
+++ src/services/webserver/app.py
@@ -6,12 +6,16 @@ import signal
 import datetime
 import sys
 
-app = Flask(__name__)
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
-signal.signal(signal.SIGTERM, lambda: sys.exit(5))
+
+def setTimezone():
+    cursor = connection.cursor()
+    cursor.execute('SET @@session.time_zone = "-6:00";')
+    connection.commit()
+    cursor.close()
+
 
 connection = mysql.connector.connect(
-    # pool_size=15,
+    pool_size=15,
     host="db",
     port=3306,
     user='root',
@@ -19,23 +23,35 @@ connection = mysql.connector.connect(
     database='libby'
 )
 
-cursor = connection.cursor()
-cursor.execute('SET @@session.time_zone = "-6:00";')
-cursor.close()
-connection.commit()
+
+app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+signal.signal(signal.SIGTERM, lambda: sys.exit(5))
+setTimezone()
 
 
 @app.route('/api/libraries/<library_id>/<offset>', methods=["POST"])
 def library_enter(library_id, offset):
-    cursor = connection.cursor()
-    new_datapoint(library_id, offset, cursor)
-    connection.commit()
-    cursor.close()
+    new_datapoint(library_id, offset)
     return '', 204
 
 
+def new_datapoint(id, offset):
+    cursor = connection.cursor()
+    statement = (
+        'INSERT INTO libby.capacity_datapoint (library_id, offset) VALUES (%s, %s)'
+    )
+    cursor.execute(statement, id, offset)
+    connection.commit()
+    cursor.close()
+
+
 @app.route('/api/locations', methods=["GET"])
-def get_fullness():
+def get_libraries():
+    return get_locations(), 200
+
+
+def get_locations():
     cursor = connection.cursor(dictionary=True)
     statement = (
         '''
@@ -48,44 +64,28 @@ def get_fullness():
         '''
     )
     cursor.execute(statement)
-    return jsonify(cursor.fetchall())
-
-
-def get_location_id(name, cursor) -> int | None:
-    statement = (
-        'SELECT id FROM libby.library WHERE name = %s'
-    )
-    cursor.execute(statement, (name,))
-    result = cursor.fetchone()
-    return result[0] if result else None
-
-
-def new_datapoint(id, offset, cursor):
-    statement = (
-        'INSERT INTO libby.capacity_datapoint (library_id, offset) VALUES (%s, %s)'
-    )
-    cursor.execute(statement, id, offset)
-
+    result = jsonify(cursor.fetchall())
+    cursor.close()
+    return result
 
 @app.route('/api/libraries/<library_id>/<date>/count', methods=["GET"])
 def get_library(library_id: str, date: str):
     if date == 'today':
         date = datetime.datetime.now().strftime("%Y-%m-%d")
 
-    cursor = connection.cursor()
     if 'time' not in request.args:
         return 'must provide ?time=seconds_since_midnight', 400
 
-    count = get_current_library_count_at(library_id, date, request.args['time'], cursor)
+    count = get_current_library_count_at(library_id, date, request.args['time'])
 
-    cursor.close()
     if count is None:
         return 'no data', 204
-    return {'count': str(count)}, 200
+    else:
+        return {'count': str(count)}, 200
 
 
 # probably doesn't work
-def get_current_library_count_at(library_id, date, time, cursor) -> int | None:
+def get_current_library_count_at(library_id, date, time) -> int | None:
     statement = (
         '''
         SELECT sum(dp.offset) FROM capacity_datapoint as dp WHERE dp.library_id = %s
@@ -94,9 +94,11 @@ def get_current_library_count_at(library_id, date, time, cursor) -> int | None:
         '''
     )
 
+    cursor = connection.cursor()
     cursor.execute(statement, (library_id, date, time))
-    result = cursor.fetchone()
-    return result[0] if result else None
+    res = getFirst(cursor)
+    cursor.close()
+    return res
 
 
 def get_current_library_count(library_id, date, cursor) -> int | None:
@@ -108,5 +110,10 @@ def get_current_library_count(library_id, date, cursor) -> int | None:
     )
 
     cursor.execute(statement, (library_id, date))
+    getFirst(cursor)
+
+
+def getFirst(cursor):
     result = cursor.fetchone()
     return result[0] if result else None
+
--- src/services/webserver/query.sh
+++ src/services/webserver/query.sh
@@ -1,4 +1,4 @@
 wget \
-  --method=GET\
+  --method=POST\
   -O - \
-  http://localhost:8000/api/locations
+  http://localhost:8000/api/libraries/6/-1
