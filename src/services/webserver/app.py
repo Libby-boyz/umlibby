506a6aa3789910574673d6e8de1d801dad61d65b
sossh <112524005+sossh@users.noreply.github.com>
2026-02-22 22:25:12 -0600

    DB changes / trends endpoint
    Created methods for interfacing with database more cleanly
    
    setup query/api endpoint for hourly trends histogram i plan to make
    
    also added some testing capasity_datapoints

--- src/services/webserver/app.py
+++ src/services/webserver/app.py
@@ -1,11 +1,14 @@
 from flask_cors import CORS
 from flask import Flask, request, jsonify
+from time import sleep
 import mysql.connector
 import os
 import signal
 import datetime
 import sys
 
+
+MAX_RETRIES = 10
 
 def setTimezone():
     cursor = connection.cursor()
@@ -30,6 +33,72 @@ signal.signal(signal.SIGTERM, lambda: sys.exit(5))
 setTimezone()
 
 
+
+#---------------------------------------#
+# General Query Methods
+#---------------------------------------#
+def execute_query_read_only(query:str, params:tuple, output_func=None, use_dict=False):
+    tries = 0
+    cursor = None
+    while tries < MAX_RETRIES:
+        try:
+
+            cursor = connection.cursor(dictionary=use_dict)
+            cursor.execute(query, params)
+            output = None
+            if output_func:
+                output = output_func(cursor)
+            cursor.close()
+            return output
+        
+        except mysql.connector.Error:
+
+            # Make sure we actually got a cursor before closing
+            if cursor:
+                cursor.close()
+            sleep(0.001)
+            tries+=1
+    
+    return None
+
+def execute_query(query:str, params:tuple, output_func=None, use_dict=False):
+    tries = 0
+    cursor = None
+    while tries < MAX_RETRIES:
+        try:
+
+            cursor = connection.cursor(dictionary=use_dict)
+            cursor.execute(query, params)
+            connection.commit()
+            output = None
+            if output_func:
+                output = output_func(cursor)
+            cursor.close()
+            return output
+        
+        except mysql.connector.Error:
+
+            # Make sure we actually got a cursor before closing
+            if cursor:
+                cursor.close()
+            sleep(0.001)
+            tries+=1
+    
+    return None
+
+def get_first(cursor):
+    result = cursor.fetchone()
+    return result[0] if result else None
+
+def get_all(cursor):
+    result = cursor.fetchall()
+    return result if result else None
+
+
+
+#---------------------------------------#
+# API Routes
+#---------------------------------------#
 @app.route('/api/libraries/<library_id>/<offset>', methods=["POST"])
 def library_enter(library_id, offset):
     new_datapoint(library_id, offset)
@@ -37,7 +106,7 @@ def library_enter(library_id, offset):
 
 
 def new_datapoint(id, offset):
-    cursor = connection.cursor()
+    #cursor = connection.cursor()
     statement = (
         '''
         INSERT INTO libby.capacity_datapoint(library_id, offset)
@@ -49,18 +118,20 @@ def new_datapoint(id, offset):
         );
         '''
     )
-    cursor.execute(statement, (id, offset, id, int(offset)))
-    connection.commit()
-    cursor.close()
+    execute_query(statement, (id, offset, id, int(offset)))
+    #cursor.execute(statement, (id, offset, id, int(offset)))
+    #connection.commit()
+    #cursor.close()
+
+
 
 
 @app.route('/api/locations', methods=["GET"])
 def get_libraries():
     return get_locations(), 200
 
-
 def get_locations():
-    cursor = connection.cursor(dictionary=True)
+    #cursor = connection.cursor(dictionary=True)
     statement = (
         '''
         SELECT lib.*, cc.fullness AS fullness
@@ -69,14 +140,18 @@ def get_locations():
             ON cc.library_id = lib.id
         '''
     )
-    cursor.execute(statement)
-    result = jsonify(cursor.fetchall())
-    cursor.close()
+    #cursor.execute(statement)
+    #result = jsonify(cursor.fetchall())
+    #cursor.close()
+    result = jsonify(execute_query_read_only(statement, (), get_all, use_dict=True))
     return result
 
 
-'''
-'''
+
+
+
+
+
 @app.route('/api/libraries/<library_id>/<date>/count', methods=["GET"])
 def get_library(library_id: str, date: str):
     if date == 'today':
@@ -103,25 +178,75 @@ def get_current_library_count_at(library_id, date, time) -> int | None:
         '''
     )
 
-    cursor = connection.cursor()
-    cursor.execute(statement, (library_id, date, time))
-    res = getFirst(cursor)
-    cursor.close()
-    return res
+    #cursor = connection.cursor()
+    #cursor.execute(statement, (library_id, date, time))
+    #res = getFirst(cursor)
+    #cursor.close()
+    return execute_query_read_only(statement, (library_id, date, time), get_first)
 
 
-def get_current_library_count(library_id, date, cursor) -> int | None:
-    statement = (
+
+#def get_current_library_count(library_id, date, cursor) -> int | None:
+#    statement = (
+#        '''
+#        SELECT sum(dp.offset) FROM capacity_datapoint as dp
+#        WHERE dp.library_id = %s AND DATE(dp.time) = %s;
+#        '''
+#    )
+#
+#    cursor.execute(statement, (library_id, date))
+#    getFirst(cursor)
+#
+#
+#def getFirst(cursor):
+#    result = cursor.fetchone()
+#    return result[0] if result else None
+
+
+@app.route('/api/libraries/<library_id>/hourly-trends', methods=["GET"])
+def hourly_trends(library_id):
+    result = get_avg_count_per_hour(library_id)
+    if not result:
+        return 'no data', 204
+    else:
+        return jsonify(result), 200
+
+
+def get_avg_count_per_hour(library_id):
+    statment = '''
+        -- Get number of people entering library for each of each day
+        WITH daily_counts as (
+        SELECT SUM(dp.offset) as count, 
+        DATE(dp.time) as date_logged,
+        HOUR(dp.time) as hour_logged
+        FROM capacity_datapoint as dp
+        WHERE library_id = %s AND dp.offset > 0
+        GROUP BY date_logged, hour_logged
+        )
+
+        -- Calculate average people per day and group by hour
+        SELECT CAST(avg(daily_counts.count) as DOUBLE) as average, hour_logged as hour
+        FROM daily_counts
+        GROUP BY hour_logged
         '''
-        SELECT sum(dp.offset) FROM capacity_datapoint as dp
-        WHERE dp.library_id = %s AND DATE(dp.time) = %s;
-        '''
-    )
+    
+    result = execute_query_read_only(statment, (library_id,), get_all, use_dict=True)
+    if not result:
+        return None
 
-    cursor.execute(statement, (library_id, date))
-    getFirst(cursor)
+    # Swap hour num (0-23) to display names (12am-11pm)
+    for i in range(len(result)):
 
+        hour = int(result[i]['hour'])
+        if hour == 0:
+            display_name = "12am"
+        elif hour < 12:
+            display_name = f"{hour}am"
+        elif hour == 12:
+            display_name = "12pm"
+        else:
+            display_name = f"{int(hour)%12}pm"
 
-def getFirst(cursor):
-    result = cursor.fetchone()
-    return result[0] if result else None
+        result[i]['hour'] = display_name
+
+    return result
